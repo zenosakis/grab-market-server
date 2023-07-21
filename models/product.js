da32ad3c022dfe81a38713c5844f92b5c2d597d6
dragonyears <dragonyears@hanil.com>
2023-07-21 13:34:16 +0900

    서버 완성(배포 생략)

---
+++ models/product.js
@@ -0,0 +1,30 @@
+module.exports = function (sequelize, DataTypes) {
+  const product = sequelize.define("Product", {
+    name: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    seller: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING(300),
+      allowNull: false,
+    },
+    imageUrl: {
+      type: DataTypes.STRING(300),
+      allowNull: true,
+    },
+    soldout: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+  });
+  return product;
+};
